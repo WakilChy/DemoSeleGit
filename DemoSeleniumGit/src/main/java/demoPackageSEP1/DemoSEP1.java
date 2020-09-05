63f8feeee57b6a93d7275cca423863f0297bc8d6
WAKIL <WAKIL@sumaiya>
2020-09-05 12:02:30 -0400

    3rd

--- DemoSeleniumGit/src/main/java/demoPackageSEP1/DemoSEP1.java
+++ DemoSeleniumGit/src/main/java/demoPackageSEP1/DemoSEP1.java
@@ -1,9 +1,19 @@
 package demoPackageSEP1;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DemoSEP1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver 85.0.4183.87\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		
+		//Launch the browser 
+		driver.get("http://www.leaftaps.com/opentaps/control/login");
+		
+		
 
 	}
 
