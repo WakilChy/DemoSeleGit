71bb372ed8d32dd985d480175a84788c9a523608
WAKIL <WAKIL@sumaiya>
2020-09-05 12:00:46 -0400

    2nd

---
+++ DemoSeleniumGit/src/main/java/demoPackageSEP1/DemoSEP1.java
@@ -0,0 +1,10 @@
+package demoPackageSEP1;
+
+public class DemoSEP1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DemoSeleniumGit/src/main/java/demoPackageSEP1/HelloDemoSele.java
@@ -0,0 +1,32 @@
+package demoPackageSEP1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloDemoSele {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver 85.0.4183.87\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		
+		//Launch the browser 
+		driver.get("http://www.leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+		
+		
+		
+		//Input UserName & password
+		driver.findElement(By.xpath("//input[@id=\"username\"]")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		
+		
+		//Click login 
+		driver.findElement(By.xpath("//input[@type=\"submit\"]")).click();
+		
+
+	}
+
+}
